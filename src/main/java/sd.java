843496b2feeed95949f4a6629e3ab7e5e66dc3e9
Willem Mulder <willemmaster@hotmail.com>
2014-07-24 09:53:49 +0200

    Add license

--- src/main/java/sd.java
+++ src/main/java/sd.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (C) 2010-2014 Mojang AB
+ *
+ * Parts of this program are marked with "VanillaIRC". These will be referred
+ * to as "Modification" from this point on.
+ *
+ * Modification Copyright (C) 2014 MinecraftOnline
+ *
+ * The Modification is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Modification is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import com.google.common.base.Charsets;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
